0e279e19b48403dbc2cb577b257b56c389ea86d1
Edison Lamar <elamar@uoc.edu>
2025-11-03 11:40:19 +0100

    Add Terraform configuration for development environment, including main.tf and variables.tf files for network and compute modules.

---
+++ infrastructure/environments/dev/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" { type = string }
+variable "region" { type = string }
+variable "zone" { type = string }
+
+variable "network_name" { type = string }
+variable "subnet_name" { type = string }
+variable "subnet_ip_cidr_range" { type = string }
+
+variable "instance_name" { type = string }
+variable "instance_machine_type" { type = string }
+variable "instance_source_image" { type = string }
+
+
